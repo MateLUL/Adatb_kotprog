896dd4702ee4149c0d65d5c0e37f02b233f29030
Csíkos Martin Nándor <h256461@stud.u-szeged.hu>
2023-11-08 19:10:10 +0100

    index hozzaadasa

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Főoldal - Ingatlan</title>
+</head>
+
+<body>
+    <a href="bejelentkezes_oldal.php">Bejelentkezés</a>
+    <br>
+    <a href="regisztracio_oldal.php">Regisztráció</a>
+</body>
+
+</html>
