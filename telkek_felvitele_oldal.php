0a263e5578340f2bf9f7fe1d6a4fc7b01f8f7d5c
Csíkos Martin Nándor <h256461@stud.u-szeged.hu>
2023-11-16 17:07:58 +0100

    php athelyezes

--- telkek_felvitele_oldal.php
+++ telkek_felvitele_oldal.php
@@ -1,3 +1,9 @@
+<?php
+    require "./php/session.php";
+    startSession();
+    checkSession();
+?>
+
 <!DOCTYPE html>
 <html lang="hu">
 
@@ -5,12 +11,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Telkek felvitele - Ingatlan</title>
-
-    <?php
-        require "./php/session.php";
-        startSession();
-        checkSession();
-    ?>
 </head>
 
 <body>
