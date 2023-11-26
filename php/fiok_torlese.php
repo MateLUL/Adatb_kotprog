06cf69072af895a9b50cfa5d70b1d8e4d30cd9ed
Csíkos Martin Nándor <h256461@stud.u-szeged.hu>
2023-11-26 16:31:56 +0100

    fiok torles

--- php/fiok_torlese.php
+++ php/fiok_torlese.php
@@ -3,15 +3,17 @@ require "./sql_csatlakozas.php";
 session_start();
 
 if (isset($_POST['torles'])) {
-    $azonosito = $_SESSION['azonosito'];
-    $felhasznalo_adoszam = "DELETE FROM felhasznalo_adoszam WHERE azonosito = '$azonosito';";
-    $felhasznalo_infok = "DELETE FROM felhasznalo_infok WHERE azonosito = '$azonosito';";
-    $felhasznalo_lakcim = "DELETE FROM felhasznalo_lakcim WHERE azonosito = '$azonosito';";
-    $felhasznalo_neve = "DELETE FROM felhasznalo_neve WHERE azonosito = '$azonosito';";
-    $felhasznalo_anyja_neve = "DELETE FROM felhasznalo_anyja_neve WHERE azonosito = '$azonosito';";
-    $felhasznalo = "DELETE FROM felhasznalo WHERE azonosito = '$azonosito';";
+    $id = $_SESSION['id'];
+    $felhasznalo_adoszam = "DELETE FROM felhasznalo_adoszam WHERE id = '$id';";
+    $felhasznalo_infok = "DELETE FROM felhasznalo_infok WHERE id = '$id';";
+    $felhasznalo_lakcim = "DELETE FROM felhasznalo_lakcim WHERE id = '$id';";
+    $felhasznalo_neve = "DELETE FROM felhasznalo_neve WHERE id = '$id';";
+    $felhasznalo_anyja_neve = "DELETE FROM felhasznalo_anyja_neve WHERE id = '$id';";
+    $tulajdonos_telek = "DELETE FROM tulajdonos_telek_birtoklas WHERE f_id = '$id'";
+    $tulajdonos_ingatlan = "DELETE FROM tulajdonos_ingatlan_birtoklas WHERE f_id = '$id'";
+    $felhasznalo = "DELETE FROM felhasznalo WHERE id = '$id';";
 
-    if ($csatlakozas->query($felhasznalo_adoszam) === TRUE && $csatlakozas->query($felhasznalo_infok) === TRUE && $csatlakozas->query($felhasznalo_lakcim) === TRUE && $csatlakozas->query($felhasznalo_neve) === TRUE && $csatlakozas->query($felhasznalo_anyja_neve) === TRUE && $csatlakozas->query($felhasznalo) === TRUE) {
+    if ($csatlakozas->query($felhasznalo_adoszam) === TRUE && $csatlakozas->query($felhasznalo_infok) === TRUE && $csatlakozas->query($felhasznalo_lakcim) === TRUE && $csatlakozas->query($felhasznalo_neve) === TRUE && $csatlakozas->query($felhasznalo_anyja_neve) === TRUE && $csatlakozas->query($tulajdonos_telek) === TRUE && $csatlakozas->query($tulajdonos_ingatlan) === TRUE && $csatlakozas->query($felhasznalo) === TRUE) {
         $_SESSION['sikeres_torles'] = "sikeres_torles";
         header("Location: ./../index.php");
     }
